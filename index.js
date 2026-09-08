d5708383f4521ef546d395b64a3f4fc5a6fd1946
norrisn <norrisn.nn@googlemail.com>
2026-09-08 22:34:56 +0100

    Add custom object homepage and API retrieval

--- index.js
+++ index.js
@@ -14,7 +14,28 @@ const PRIVATE_APP_ACCESS = process.env.HUBSPOT_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const characters =
+        'https://api.hubapi.com/crm/v3/objects/2-253075463?properties=name,game,role';
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const resp = await axios.get(characters, { headers });
+        const data = resp.data.results;
+
+        res.render('homepage', {
+            title: 'Game Characters | Integrating With HubSpot I Practicum',
+            data
+        });
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Failed to retrieve Game Characters');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
